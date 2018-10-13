d771458ed6ddc62a960a5b00b6577081ca1b6145
Guest User <Guest@Alihans-MacBook-Pro.local>
2018-10-13 12:51:13 +0300

    Initial Commit

---
+++ Senfonico Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Senfonico Challenge
+//
+//  Created by Guest User on 13.10.2018.
+//  Copyright © 2018 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
